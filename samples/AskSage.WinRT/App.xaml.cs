4d3de1e62447b325912d910d27a9911b268872e7
Russell Libby <russell.libby@sage.com>
2013-06-27 12:04:04 -0700

    WinRT AskSage app

---
+++ samples/AskSage.WinRT/App.xaml.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.ApplicationModel.Resources;
+using System.Threading.Tasks;
+
+namespace AskSage.WinRT
+{
+    public class ItemsModel : INotifyPropertyChanged
+    {
+        private bool _User;
+        private string _Text;
+        private string _InputTime;
+
+        public float Lerp(float start, float end, float amount)
+        {
+            float difference = end - start;
+            float adjusted = difference * amount;
+            return start + adjusted;
+        }
+
+        public Color Lerp(Color colour, Color to, float amount)
+        {
+            // start colours as lerp-able floats
+            float sr = colour.R, sg = colour.G, sb = colour.B;
+
+            // end colours as lerp-able floats
+            float er = to.R, eg = to.G, eb = to.B;
+
+            // lerp the colours to get the difference
+            byte r = (byte)Lerp(sr, er, amount),
+                 g = (byte)Lerp(sg, eg, amount),
+                 b = (byte)Lerp(sb, eb, amount);
+
+            // return the new colour
+            return Color.FromArgb(255, r, g, b);
+        }
+
+        public ItemsModel(bool user, string text)
+        {
+            _User = user;
+            _Text = text;
+            _InputTime = DateTime.Now.ToString("ddd") + " " + DateTime.Now.ToString("t").ToLower();
+        }
+
+        public string InputTime
+        {
+            get
+            {
+                return _InputTime;
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return _Text;
+            }
+        }
+
+        public Brush BackColor
+        {
+            get
+            {
+                Color c = Color.FromArgb(0xff, 0x5F, 0x37, 0xBE);
+
+                return _User ? new SolidColorBrush(c) : new SolidColorBrush(Lerp(c, Colors.Black, 0.25f));
+            }
+        }
+
+        public HorizontalAlignment Alignment
+        {
+            get
+            {
+                return _User ? HorizontalAlignment.Right : HorizontalAlignment.Left;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<ItemsModel> _Items;
+
+        public MainViewModel()
+        {
+            _Items = new ObservableCollection<ItemsModel>();
+        }
+
+        public ObservableCollection<ItemsModel> Items
+        {
+            get
+            {
+                return _Items;
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        private static MainViewModel viewModel = null;
+
+        public static List<string> GrammerList = new List<string>();
+
+        /// <summary>
+        /// A static ViewModel used by the views to bind against.
+        /// </summary>
+        /// <returns>The MainViewModel object.</returns>
+        public static MainViewModel ViewModel
+        {
+            get
+            {
+                // Delay creation of the view model until necessary
+                if (viewModel == null)
+                {
+                    viewModel = new MainViewModel();
+                }
+
+                return viewModel;
+            }
+        }
+
+        private async void LoadGrammer()
+        {
+            string line;
+
+            var uri = new Uri("ms-appx:///Assets/Grammer.txt");
+
+            var file = StorageFile.GetFileFromApplicationUriAsync(uri).AsTask();
+            await file;
+            
+            var data = file.Result.OpenAsync(FileAccessMode.Read).AsTask();
+            await data;
+
+            using (var reader = new StreamReader(data.Result.AsStreamForRead()))
+            {
+                // Read the lines
+                using (StringReader sr = new StringReader(reader.ReadToEnd()))
+                {
+                    // While not eof
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        // Check line
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            // Add line to grammer list
+                            GrammerList.Add(line);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+
+            LoadGrammer();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ samples/AskSage.WinRT/MainPage.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Animation;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using Windows.ApplicationModel.Resources;
+using System.Threading.Tasks;
+
+namespace AskSage.WinRT
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        internal class UiDispatcher
+        {
+            private MainPage _Page;
+            private string _Text = string.Empty;
+            private bool _User = false;
+
+            public UiDispatcher(MainPage page, bool user, string text)
+            {
+                _Page = page;
+                _Text = text;
+                _User = user;
+            }
+
+            public UiDispatcher(MainPage page)
+            {
+                _Page = page;
+            }
+
+            public void UpdateState()
+            {
+                // Show or hide the progress bar
+                _Page.progress.Visibility = (_Page._Connected && !_Page._Waiting) ? Visibility.Collapsed : Visibility.Visible;
+
+                // Enable or disable the text box
+                _Page.request.IsEnabled = (_Page._Connected && !_Page._Waiting);
+            }
+
+            public void AddConversationText()
+            {
+                // Create new item model
+                ItemsModel item = new ItemsModel(_User, _Text);
+
+                // Add to the view model
+                App.ViewModel.Items.Add(item);
+
+                // Make sure its scrolled into view
+                _Page.conversation.ScrollIntoView(item);
+            }
+                
+        }
+
+        private HubConnection _Connection;
+        private IHubProxy _Hub;
+        private bool _Connected = false;
+        private bool _Welcome = false;
+        private bool _Waiting = false;
+        private bool _Speech = true;
+        private bool _Demo = true;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Set the data context for the page bindings
+            DataContext = App.ViewModel;
+        }
+
+        private async void ReportChange(StateChange change)
+        {
+            UiDispatcher disp = new UiDispatcher(this, false, "How can I assist you?");
+
+            // Set connected state
+            _Connected = (change.NewState == ConnectionState.Connected);
+
+            // If connected and not welcomed yet
+            if (_Connected && !_Welcome)
+            {
+                // Set state
+                _Welcome = true;
+                // Add text
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(disp.AddConversationText));
+            }
+
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(disp.UpdateState));
+        }
+
+        /* Handle response text from system */
+        private async void OnResponse(string data)
+        {
+            UiDispatcher state = new UiDispatcher(this);
+
+            // Check data
+            if (data != null)
+            {
+                UiDispatcher disp = new UiDispatcher(this, false, data);
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(disp.AddConversationText));
+            }
+
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(state.UpdateState));
+        }
+
+        private void ReportClosed()
+        {
+            _Connection.Start();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            UiDispatcher state = new UiDispatcher(this);
+
+            // Update the UI state
+            state.UpdateState();
+
+            // Create connection
+            _Connection = new HubConnection("http://swmsignalrsite.azurewebsites.net/");
+            _Connection.StateChanged += change => ReportChange(change);
+            _Connection.Closed += () => ReportClosed();
+
+            // Create hub proxy
+            _Hub = _Connection.CreateHubProxy("erpTicker");
+            _Hub.On<string>("addResponse", data => OnResponse(data));
+
+            // Start the connection
+            _Connection.Start();
+        }
+
+        private void request_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            // Check for enter key
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                // Set the key to handled
+                e.Handled = true;
+                // Fire the event handler
+                ask_Click(sender, e);
+            }
+        }
+
+        private void ask_Click(object sender, RoutedEventArgs e)
+        {
+            UiDispatcher disp = new UiDispatcher(this, true, request.Text);
+
+            // Check state with SignalR hub
+            if (_Connected && !_Waiting)
+            {
+                // If text
+                if (!string.IsNullOrEmpty(request.Text))
+                {
+                    // Add the text
+                    disp.AddConversationText();
+
+                    // Invoke the request
+                    _Hub.Invoke("sendRequest", new object[] { _Connection.ConnectionId, request.Text });
+
+                    // Clear the text
+                    request.Text = string.Empty;
+                }
+            }
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear the items
+            App.ViewModel.Items.Clear();
+        }
+
+        private void conversation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            conversation.SelectedIndex = (-1);
+        }
+    }
+}
